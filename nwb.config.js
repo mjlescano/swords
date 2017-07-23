8ae7980d8bdd22fcfc10310b9be2b02fc676173a
Matías Lescano <mjlescano@protonmail.com>
2017-07-23 15:30:32 -0300

    add static file serving

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  type: 'web-app'
+  type: 'web-app',
+  webpack: {
+    host: '192.168.1.4'
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+const path = require('path')
 const { createServer } = require('http')
 const { Server } = require('colyseus')
+const nodeStatic = require('node-static')
 const SwordsRoom = require('./room')
 
-const httpServer = module.exports = createServer()
+const staticServer = new nodeStatic.Server(path.resolve(__dirname, '..', 'dist'))
+
+const httpServer = module.exports = createServer((req, res) => {
+  req.addListener('end', function () {
+    staticServer.serve(req, res)
+  }).resume()
+})
 
 const gameServer = new Server({
   server: httpServer
